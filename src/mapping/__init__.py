e1b4960792f3e09fe1b3829a9f9ba66713b5f92f
Sinclert Pérez <Sinclert@hotmail.com>
2021-03-01 10:59:06 -0500

    Define data record types

--- src/mapping/__init__.py
+++ src/mapping/__init__.py
@@ -7,3 +7,27 @@ from .record_adapters import JargonGroupAdapter
 
 from .record_mappers import BaseRecordMapper
 from .record_mappers import IDRecordMapper
+
+from .record_types import *
+
+
+ADAPTERS = {
+    TYPE_CATEGORY.name: CategoryAdapter,
+    TYPE_GROUP.name: JargonGroupAdapter,
+    TYPE_JARGON.name: JargonAdapter,
+}
+
+
+def select_adapter(type_name: str) -> BaseAdapter:
+    """
+    Selects the corresponding record adapter given a data model type
+    :param type_name: name of the data model
+    :return: record adapter
+    """
+
+    try:
+        adapter_cls = ADAPTERS[type_name]
+        adapter_obj = adapter_cls()  # type: ignore
+        return adapter_obj
+    except KeyError:
+        raise ValueError(f"Invalid model type: {type_name}")
