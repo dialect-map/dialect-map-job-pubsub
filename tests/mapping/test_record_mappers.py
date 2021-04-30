038036d319885fb7d27e37845cff9ec2d8a3c762
Sinclert Pérez <Sinclert@hotmail.com>
2021-04-30 11:20:18 -0400

    Refactor record mapper tests

--- tests/mapping/test_record_mappers.py
+++ tests/mapping/test_record_mappers.py
@@ -2,58 +2,76 @@
 
 import pytest
 
-from src.mapping import BaseRecordMapper
 from src.mapping import FieldRecordMapper
 from src.mapping import TYPE_CATEGORY
 from src.mapping import TYPE_JARGON
 from src.mapping import TYPE_GROUP
 
 
-@pytest.fixture(scope="module")
-def mapper() -> BaseRecordMapper:
-    """
-    Data record to data model name mapper
-    :return: record mapper object
-    """
+class TestCategoryFieldMapper:
+    """Class to group all category field mapper tests"""
 
-    return FieldRecordMapper("prop", [TYPE_CATEGORY, TYPE_JARGON, TYPE_GROUP])
+    @pytest.fixture(scope="class")
+    def mapper(self):
+        """
+        Data record to data model name mapper
+        :return: record mapper object
+        """
+
+        return FieldRecordMapper("prop", [TYPE_CATEGORY])
+
+    def test_field_mapper_valid(self, mapper: FieldRecordMapper):
+        """
+        Test the correct type mapping of valid samples
+        :param mapper: record mapper object
+        """
+
+        assert mapper.infer_type({"prop": "top"}) == TYPE_CATEGORY.name
+        assert mapper.infer_type({"prop": "top-suffix"}) == TYPE_CATEGORY.name
+        assert mapper.infer_type({"prop": "top-suffix.low"}) == TYPE_CATEGORY.name
+        assert mapper.infer_type({"prop": "top-suffix.low-suffix"}) == TYPE_CATEGORY.name
+
+    def test_field_mapper_error(self, mapper: FieldRecordMapper):
+        """
+        Test the correct type mapping of valid samples
+        :param mapper: record mapper object
+        """
+
+        assert pytest.raises(ValueError, mapper.infer_type, {"prop": "wrong."})
+        assert pytest.raises(ValueError, mapper.infer_type, {"prop": "wrong.wrong-"})
+        assert pytest.raises(ValueError, mapper.infer_type, {"prop": "wrong-wrong."})
 
 
-@pytest.mark.parametrize(
-    ["sample", "expected"],
-    [
-        ({"prop": "cs.AI"}, TYPE_CATEGORY.name),
-        ({"prop": "group-1"}, TYPE_GROUP.name),
-        ({"prop": "group-123"}, TYPE_GROUP.name),
-        ({"prop": "group-1-jargon-1"}, TYPE_JARGON.name),
-        ({"prop": "group-1-jargon-123"}, TYPE_JARGON.name),
-    ],
-)
-def test_prop_mapper_infer(mapper: FieldRecordMapper, sample: dict, expected: str):
-    """
-    Test the correct type mapping of valid samples
-    :param mapper: record mapper object
-    :param sample: record sample dictionary
-    :param expected: record corresponding data type
-    """
+class TestJargonFieldMapper:
+    """Class to group all jargon types field mapper tests"""
 
-    assert mapper.infer_type(sample) == expected
+    @pytest.fixture(scope="class")
+    def mapper(self):
+        """
+        Data record to data model name mapper
+        :return: record mapper object
+        """
 
+        return FieldRecordMapper("prop", [TYPE_JARGON, TYPE_GROUP])
 
-@pytest.mark.parametrize(
-    "sample",
-    [
-        {"prop": "wrong"},
-        {"prop": "groups"},
-        {"prop": "group-X"},
-        {"prop": "group-1-jargon-X"},
-    ],
-)
-def test_prop_mapper_infer_invalid(mapper: FieldRecordMapper, sample: dict):
-    """
-    Test the correct exception raised when invalid samples
-    :param mapper: record mapper object
-    :param sample: record sample dictionary
-    """
+    def test_field_mapper_valid(self, mapper: FieldRecordMapper):
+        """
+        Test the correct type mapping of valid samples
+        :param mapper: record mapper object
+        """
 
-    assert pytest.raises(ValueError, mapper.infer_type, sample)
+        assert mapper.infer_type({"prop": "group-1"}) == TYPE_GROUP.name
+        assert mapper.infer_type({"prop": "group-123"}) == TYPE_GROUP.name
+        assert mapper.infer_type({"prop": "group-1-jargon-1"}) == TYPE_JARGON.name
+        assert mapper.infer_type({"prop": "group-1-jargon-123"}) == TYPE_JARGON.name
+
+    def test_field_mapper_error(self, mapper: FieldRecordMapper):
+        """
+        Test the correct type mapping of valid samples
+        :param mapper: record mapper object
+        """
+
+        assert pytest.raises(ValueError, mapper.infer_type, {"prop": "wrong"})
+        assert pytest.raises(ValueError, mapper.infer_type, {"prop": "groups"})
+        assert pytest.raises(ValueError, mapper.infer_type, {"prop": "group-X"})
+        assert pytest.raises(ValueError, mapper.infer_type, {"prop": "group-1-jargon-X"})
