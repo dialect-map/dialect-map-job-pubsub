6fa6ee9f82f69f6cfb04483f9a650bedaaf68743
Sinclert Pérez <Sinclert@hotmail.com>
2021-07-07 18:02:31 +0200

    Move API operators to 'output' folder

--- src/operators/__init__.py
+++ src/operators/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from .api import BaseAPIOperator
-from .api import DialectMapOperator
-
 from .message import BaseMessageOperator
 from .message import DiffMessageOperator
