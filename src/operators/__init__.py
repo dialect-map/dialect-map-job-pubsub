ed8071d851c548f7d83588360962f287a831eda0
Sinclert Pérez <Sinclert@hotmail.com>
2021-07-07 12:58:32 +0200

    Move PubSub operator to 'input' folder

--- src/operators/__init__.py
+++ src/operators/__init__.py
@@ -3,8 +3,5 @@
 from .api import BaseAPIOperator
 from .api import DialectMapOperator
 
-from .pubsub import BasePubSubOperator
-from .pubsub import DiffPubSubOperator
-
 from .message import BaseMessageOperator
 from .message import DiffMessageOperator
