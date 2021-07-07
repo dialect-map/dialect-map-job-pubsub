6646bc3d9a3353260634d9d7aa7853360d9fa6f2
Sinclert Pérez <Sinclert@hotmail.com>
2021-07-07 18:06:03 +0200

    Remove record adapter classes

--- src/mapping/__init__.py
+++ src/mapping/__init__.py
@@ -2,11 +2,6 @@
 
 from models import *
 
-from .record_adapters import BaseAdapter
-from .record_adapters import CategoryAdapter
-from .record_adapters import JargonAdapter
-from .record_adapters import JargonGroupAdapter
-
 from .record_mappers import BaseRecordMapper
 from .record_mappers import DummyRecordMapper
 from .record_mappers import FieldRecordMapper
@@ -15,12 +10,6 @@ from .record_routes import CATEGORY_ROUTE
 from .record_routes import GROUP_ROUTE
 from .record_routes import JARGON_ROUTE
 
-
-TYPE_ADAPTERS = {
-    TYPE_CATEGORY.name: CategoryAdapter,
-    TYPE_GROUP.name: JargonGroupAdapter,
-    TYPE_JARGON.name: JargonAdapter,
-}
 
 FILE_MAPPERS = {
     FILE_CATEGORY.name: FieldRecordMapper,
@@ -32,21 +21,6 @@ API_ROUTES = {
     TYPE_GROUP.name: GROUP_ROUTE,
     TYPE_JARGON.name: JARGON_ROUTE,
 }
-
-
-def init_adapter(type_name: str) -> BaseAdapter:
-    """
-    Selects the corresponding record adapter given a data type name
-    :param type_name: name of the data type
-    :return: record adapter
-    """
-
-    try:
-        adapter_cls = TYPE_ADAPTERS[type_name]
-        adapter_obj = adapter_cls()  # type: ignore
-        return adapter_obj
-    except KeyError:
-        raise ValueError(f"Invalid model type: {type_name}")
 
 
 def init_mapper(source_file: str, **kwargs) -> BaseRecordMapper:
