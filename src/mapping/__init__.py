80865af3ca19c41501924e4e7e9cc39b2cd3e3d2
Sinclert Pérez <Sinclert@hotmail.com>
2021-07-15 11:02:38 +0200

    Refactor record mapper classes

--- src/mapping/__init__.py
+++ src/mapping/__init__.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from models import *
+from .record_mapping import BaseRecordMapper
+from .record_mapping import SchemaRecordMapper
 
 from .record_routes import CATEGORY_ROUTE
 from .record_routes import GROUP_ROUTE
 from .record_routes import JARGON_ROUTE
 
 
-API_ROUTES = {
-    TYPE_CATEGORY.name: CATEGORY_ROUTE,
-    TYPE_GROUP.name: GROUP_ROUTE,
-    TYPE_JARGON.name: JARGON_ROUTE,
+ROUTES = {
+    "categories.json": [
+        CATEGORY_ROUTE,
+    ],
+    "jargons.json": [
+        GROUP_ROUTE,
+        JARGON_ROUTE,
+    ],
 }
+
+
+def init_all_mappers() -> dict:
+    """
+    Initializes all data file mappers
+    :return: dictionary file - mapper
+    """
+
+    return {file_name: SchemaRecordMapper(routes) for file_name, routes in ROUTES.items()}
