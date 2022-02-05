d0b6660c97b3cd7b6e89daacefaf8bc08ef3abaa
Sinclert <sinclert@hotmail.com>
2022-02-05 11:03:22 +0100

    Remove legacy jobs/mapping code

--- src/job/mapping/__init__.py
+++ src/job/mapping/__init__.py
@@ -6,23 +6,3 @@ from .record_mapping import SchemaRecordMapper
 from .record_routes import CATEGORY_ROUTE
 from .record_routes import GROUP_ROUTE
 from .record_routes import JARGON_ROUTE
-
-
-ROUTES = {
-    "categories.json": [
-        CATEGORY_ROUTE,
-    ],
-    "jargons.json": [
-        GROUP_ROUTE,
-        JARGON_ROUTE,
-    ],
-}
-
-
-def init_all_mappers() -> dict:
-    """
-    Initializes all data file mappers
-    :return: dictionary file - mapper
-    """
-
-    return {file_name: SchemaRecordMapper(routes) for file_name, routes in ROUTES.items()}
